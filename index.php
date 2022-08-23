1be30f09a62c3ceb86902388be0b46001c14be2d
Antonius131 <antomendetta@gmail.com>
2022-08-23 11:31:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Matches</title>
+</head>
+<body>
+   <?php 
+      $matches = [
+         [
+            'home' => 'Detroit Pistons',
+            'away' => 'Golden State Warriors',
+            'home points' => '88',
+            'away points' => '121'
+         ],
+         [
+            'home' => 'Los Angeles Lakers',
+            'away' => 'Chicago Bulls',
+            'home points' => '79',
+            'away points' => '99'
+         ],
+         [
+            'home' => 'New York Knicks',
+            'away' => 'Cleveland Cavaliers',
+            'home points' => '88',
+            'away points' => '121'
+         ],
+         [
+            'home' => 'San Antonio Spurs',
+            'away' => 'Boston Celtics',
+            'home points' => '88',
+            'away points' => '121'
+         ],
+         [
+            'home' => 'Miami Heat',
+            'away' => 'Dallas Mavericks',
+            'home points' => '88',
+            'away points' => '121'
+         ],
+      ]
+   ?>
+</body>
+</html>
