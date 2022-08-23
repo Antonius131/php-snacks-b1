abb11292b83af519a53fef0dab0f53fd82368e6f
Antonius131 <antomendetta@gmail.com>
2022-08-23 14:57:20 +0200

    Snack 3: create conditions for $numbers array

--- index.php
+++ index.php
@@ -93,10 +93,12 @@
    <?php 
       $numbers = [];
       
-
       while (count($numbers) < 15) {
          $randomNumber = rand(1,100);
 
+         if (!in_array($randomNumber, $numbers)) {
+            $numbers[] = $randomNumber;
+         }
       }
 
       var_dump($numbers);
